0532efe83c0c6b24ccd41922312f0a9a1e961191
Yue Zhuo <zyue1105@gmail.com>
2013-04-14 14:16:51 -0600

    store inbox email: [date, from, subject, content]

--- get_emails.py
+++ get_emails.py
@@ -1,15 +1,22 @@
-import email, getpass, imaplib, os, sys
+import email
+import getpass
+import imaplib
+import os
+import sys
 
 detach_dir = '.' # directory where to save attachments (default current)
-user = raw_input('Enter youe Gmail username: ')
-pwd = getpass.getpass('Enter your password: ')
+#user = raw_input('Enter youe Gmail username: ')
+user = '670information'
+#pwd = getpass.getpass('Enter your password: ')
+pwd = 'bingorgoogle'
 
 # connecting to the gmail imap server
 m = imaplib.IMAP4_SSL('imap.gmail.com')
 m.login(user, pwd)
 
 # here you a can choose a mail box like INBOX instead
-m.select('[Gmail]/All Mail') 
+#m.select('[Gmail]/All Mail')
+m.select('inbox') 
 # use m.list() to get all the mailboxes
 
 ''' you could filter using the IMAP rules here
@@ -18,6 +25,9 @@ m.select('[Gmail]/All Mail')
 resp, items = m.search(None, 'ALL') 
 items = items[0].split() # getting the mails id
 
+mails = []
+
+emails = []
 for emailid in items:
     ''' fetching the mail,`(RFC822)` means get the whole stuff,
     but you can ask for headers only, etc
@@ -25,38 +35,12 @@ for emailid in items:
     resp, data = m.fetch(emailid, '(RFC822)') 
     email_body = data[0][1] # getting the mail content
     # parsing the mail content to get a mail object
-    mail = email.message_from_string(email_body) 
+    mail = email.message_from_string(email_body)
+    
+    curr_mail = {}
+    curr_mail['From'] = mail['From']
+    curr_mail['Date'] = mail['Date']
+    curr_mail['Subject'] = mail['Subject']
+    curr_mail['Content'] = mail.get_payload()
 
-    #Check if any attachments at all
-    if mail.get_content_maintype() != 'multipart':
-        continue
-    print mail
-    #print '[' + mail['From'] + '] :' + mail['Subject']
-
-    ''' we use walk to create a generator so we can iterate
-    on the parts and forget about the recursive headach '''
-    for part in mail.walk():
-        # multipart are just containers, so we skip them
-        if part.get_content_maintype() == 'multipart':
-            continue
-
-        # is this part an attachment 
-        if part.get('Content-Disposition') is None:
-            continue
-
-        filename = part.get_filename()
-        counter = 1
-
-        # if there is no filename, we create one with a counter to avoid duplicates
-        if not filename:
-            filename = 'part-%03d%s' % (counter, 'bin')
-            counter += 1
-
-        att_path = os.path.join(detach_dir, filename)
-
-        # Check if its already there
-        if not os.path.isfile(att_path): 
-            # finally write the stuff
-            fp = open(att_path, 'wb')
-            fp.write(part.get_payload(decode=True))
-            fp.close()
+    emails.append(curr_mail)
